b842c89327d952a4202f5d64443466b7d22b7641
Minh Dinh Nguyen <minhnd0496@gmail.com>
2021-07-21 23:47:12 +0700

    in progress2

--- src/layouts/header/header.component.jsx
+++ src/layouts/header/header.component.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
-import ReactLogo from '../../../public/bds_logo.svg'
+import { ReactComponent as ReactLogo } from 'public/bds_logo.svg'
 import useStyles from './header.styles'
 
 const Header = (props) => {
