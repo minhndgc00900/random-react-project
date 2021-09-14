2dcaaf2204c98715c66d7db2eac64d39d48f758f
Minh Dinh Nguyen <minhnd0496@gmail.com>
2021-09-14 21:34:20 +0700

    done slider

--- src/data/hot-properties.js
+++ src/data/hot-properties.js
@@ -19,6 +19,21 @@ const hotPropertyData = [
 		image:
 			'https://file4.batdongsan.com.vn/crop/260x146/2021/06/08/20210608110725-cca6.jpg',
 	},
+	{
+		title: 'Moonlight Centre Point',
+		image:
+			'https://file4.batdongsan.com.vn/crop/260x146/2020/12/04/hmcVYWuR/20201204134139-fb5e.jpg',
+	},
+	{
+		title: 'Moonlight Centre Point',
+		image:
+			'https://file4.batdongsan.com.vn/crop/260x146/2020/10/13/hmcVYWuR/20201013133153-6a58.jpg',
+	},
+	{
+		title: 'Moonlight Centre Point',
+		image:
+			'https://file4.batdongsan.com.vn/crop/260x146/2021/06/08/20210608110725-cca6.jpg',
+	},
 ]
 
 export { hotPropertyData }
--- src/pages/home-page/hot-property/hot-property.component.jsx
+++ src/pages/home-page/hot-property/hot-property.component.jsx
@@ -3,6 +3,9 @@ import Slider from 'react-slick'
 import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
 import './hot-property.styles.scss'
+import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos'
+import { Grid } from '@material-ui/core'
+import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos'
 
 function HotProperty({ data }) {
 	console.log(123, data)
@@ -11,31 +14,54 @@ function HotProperty({ data }) {
 		infinite: true,
 		slidesToShow: 3,
 		slidesToScroll: 1,
-		autoplay: false,
-		// autoplaySpeed: 10000,
+		autoplay: true,
+		autoplaySpeed: 4000,
 		pauseOnHover: true,
+		arrows: false,
 		className: 'slider-custom',
 	}
 
+	const onHandleBack = () => {
+		slider.slickPrev()
+	}
+
+	const onHandleForward = () => {
+		slider.slickNext()
+	}
+
+	let slider = null
+
 	return (
 		<div className='hot-property'>
-			<h2 className='title-name'>Dự án nổi bật</h2>
-			<div>
-				<Slider {...settings}>
-					{data
-						? data.map((item) => (
-								<>
-									<img
-										style={{ marginBottom: '12px' }}
-										src={item.image}
-										alt=''
-									/>
-									<span className='title-slider-item'>{item.title}</span>
-								</>
-						  ))
-						: null}
-				</Slider>
-			</div>
+			<Grid container className='slider-container'>
+				<Grid item xs={1}>
+					<ArrowBackIosIcon className='left-arrow' onClick={onHandleBack} />
+				</Grid>
+				<Grid item xs={10}>
+					<h2 className='title-name'>Dự án nổi bật</h2>
+
+					<Slider ref={(c) => (slider = c)} {...settings}>
+						{data
+							? data.map((item, key) => (
+									<div id={key}>
+										<img
+											style={{ marginBottom: '12px' }}
+											src={item.image}
+											alt=''
+										/>
+										<span className='title-slider-item'>{item.title}</span>
+									</div>
+							  ))
+							: null}
+					</Slider>
+				</Grid>
+				<Grid item xs={1}>
+					<ArrowForwardIosIcon
+						className='right-arrow'
+						onClick={onHandleForward}
+					/>
+				</Grid>
+			</Grid>
 		</div>
 	)
 }
